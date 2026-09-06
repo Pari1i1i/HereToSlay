50b901ab801d00a6dc7b7f594cc3d2a8f474d292
Pari1i1i <fachriachmad575@gmail.com>
2026-09-06 20:39:42 +0700

    placeholder and changing start route

--- app/src/main/java/com/example/heretoslay/MainActivity.kt
+++ app/src/main/java/com/example/heretoslay/MainActivity.kt
@@ -1,16 +1,13 @@
-    package com.example.heretoslay
+package com.example.heretoslay
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.heretoslay.ui.navigation.HtsNavGraph
 import com.example.heretoslay.ui.theme.HereToSlayTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +16,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HereToSlayTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    HtsNavGraph()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HereToSlayTheme {
-        Greeting("Android")
     }
 }
